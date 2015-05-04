7417ebb0dc88070ae67e8987b2eacddab08ef5d7
Lucas Gatsas <lucasgatsas@gmail.com>
2015-05-04 04:49:14 +0200

    Update

--- js/spacegstylesheets.js
+++ js/spacegstylesheets.js
@@ -8,11 +8,6 @@
 /* New Update Fr. 27. February. 2015 -21:53:19*/
 
 
-/*!
- * Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2014 Start Bootstrap
- * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
- */
 
 
  /*!
